ad10a020e2dd69092cdff28f6757d797d2624519
呂向煬 <106233747+lynickyang@users.noreply.github.com>
2022-09-09 17:15:27 +0800

    Update 2022-09-09T09:15:25.556Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,8 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+var swiper = new Swiper(".swiper-program", {
+  slidesPerView: "auto",
+  spaceBetween: 8,
+  grabCursor: true
 });
 //# sourceMappingURL=all.js.map
